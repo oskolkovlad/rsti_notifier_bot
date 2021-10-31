2ae2d7ee152ca14bc17efb14a06e972c238beb0e
Влад Осколков <osvlad@outlook.com>
2021-10-31 22:05:14 +0300

    DateTime, final battle.

--- RstiNotifierBot/Model/Entities/News.cs
+++ RstiNotifierBot/Model/Entities/News.cs
@@ -15,24 +15,9 @@
             Url = url;
             ImageUrl = imageUrl;
 
-            Console.WriteLine(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern);
-            Console.WriteLine(CultureInfo.InvariantCulture.DateTimeFormat.ShortDatePattern);
-
             var cultureInfo = new CultureInfo("ru-RU");
             if (DateTime.TryParse(date, cultureInfo, DateTimeStyles.None, out var publishDate))
             {
-                Console.WriteLine(cultureInfo.DateTimeFormat.ShortDatePattern);
-
-                var publishDate1 = DateTime.ParseExact(date.Replace("&nbsp;", null), "dd/mm/yy", cultureInfo);
-                Console.WriteLine("1 " + publishDate1.ToString("dd.mm.yy"));
-                Console.WriteLine("1 " + publishDate1.ToString("dd.mm.yyyy"));
-
-                var publishDate2 = DateTime.ParseExact(date.Replace("&nbsp;", null), "dd.mm.yy", cultureInfo);
-                Console.WriteLine("2 " + publishDate2.ToString("dd.mm.yy"));
-                Console.WriteLine("2 " + publishDate2.ToString("dd.mm.yyyy"));
-
-                Console.WriteLine();
-
                 publishDate = DateTime.ParseExact(date.Replace("&nbsp;", null), "dd.MM.yy", cultureInfo);
             }
             PublishDate = publishDate;
